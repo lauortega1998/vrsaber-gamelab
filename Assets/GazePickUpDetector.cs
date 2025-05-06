4a309018406df1649cc957b3cfa8be4923ae7c40
VikiST68 <152392508+VikiST68@users.noreply.github.com>
2025-05-06 14:38:18 +0200

    ui pickable

--- Assets/GazePickUpDetector.cs
+++ Assets/GazePickUpDetector.cs
@@ -54,5 +54,8 @@ public class GazePickupDetector : MonoBehaviour
         {
             Debug.LogWarning("PickupIndicator not found on: " + target.name);
         }
+        
+        
+        
     }
 }
